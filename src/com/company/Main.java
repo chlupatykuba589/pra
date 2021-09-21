167fc6e59c3cb2cb8df65132306e9521291a8e84
jakub.chomanic <kakacz5@seznam.cz>
2021-09-21 14:41:17 +0200

    zajda neda klik a trava

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ahoj svete");
         System.out.println("zelena je trava, zajda neda klik");
+        System.out.println("dalsi zmena");
     }
 }
